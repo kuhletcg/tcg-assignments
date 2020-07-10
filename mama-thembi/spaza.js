7e67bd5aab3bcc571fa4df86d3cdb965d2f648e2
vuyisile <vuyisilemark@gmail.com>
2020-07-10 11:24:39 +0200

    fixing

--- mama-thembi/spaza.js
+++ mama-thembi/spaza.js
@@ -7,3 +7,16 @@ function myJsFunc() {
  <a href="javascript:void(0)" onclick="myJsFunc();">Run JavaScript Code</a>
 
 
+<select name ="eyeColor" id="eyeColor" multiple>
+  <label for ="Eyecolor">Eye color</label>
+  <option value="Blue">Blue</option>
+<option label="Red" value="Red"></option>
+</div>
+<div> 
+  <label for=""bio>Bio</label>
+  <textarea id="hidden" name="hidden"  value="h1">
+</div>
+  <div>
+  <label for="file">File</label>
+  <input id="file" type="file" name="file">
+</div>
